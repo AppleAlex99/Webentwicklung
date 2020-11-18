aaeaeb0137519170a624c46ae462a24ad8b973e8
Alexander Winzig <alex.winzig@gmx.de>
2020-11-18 12:55:45 +0100

    index.php und Login.php fertig

--- Ueb01/Login.php
+++ Ueb01/Login.php
@@ -1,1 +1,44 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ToDo Liste</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/jquery@3.5.1/dist/jquery.min.js"></script>
+    <script src="https://unpkg.com/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://unpkg.com/bootstrap@4.5.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+    <div class="jumbotron text-center">
+        <h1 class="display-4">Aufgabenplaner: Login</h1>
+    </div>
+
+
+</head>
+<body>
+
+<div class="container-fluid col-10">
+
+
+    <form>
+        <div class="form-group">
+            <label for="Email">E-Mail</label>
+            <input type="email" class="form-control" id="email">
+
+            <label for="Passwort">Passwort</label>
+            <input type="password" class="form-control" id="passwort">
+
+        </div>
+        <button type="submit" class="btn btn-primary">
+            Log in
+        </button>
+    </form>
+</div>
+
+</body>
+<footer>
+
+</footer>
+</html>
--- Ueb01/Personen.php
+++ Ueb01/Personen.php
@@ -1,1 +1,73 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ToDo Liste</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/jquery@3.5.1/dist/jquery.min.js"></script>
+    <script src="https://unpkg.com/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://unpkg.com/bootstrap@4.5.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+    <div class="jumbotron text-center">
+        <h1 class="display-4">Aufgabenplaner: Todos(Aktuelles Projekt)</h1>
+    </div>
+
+
+</head>
+<body>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col col-2">
+            <!-- As a link -->
+            <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand" href="Login.php">Login............</a>
+                <a class="navbar-brand" href="Projekte.php">Projekte.........</a>
+                <a class="navbar-brand" href="index.php">Aktuelles Projekt</a>
+                <a class="navbar-brand" href="Reiter.php">Reiter...........</a>
+                <a class="navbar-brand" href="Aufgaben.php">Aufgaben.........</a>
+                <a class="navbar-brand" href="Personen.php">Mitglieder.......</a>
+
+            </nav>
+
+        </div>
+        <div class="col">
+            <div class="card-deck">
+                <div class="card">
+                    <div class="card-header">ToDo</div>
+                    <div class="list-group">
+                        <div class="list-group-item"
+                        <li>schlafen</li>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">Erledigt</div>
+                <div class="list-group">
+                    <div class="list-group-item"
+                    <li>schlafen</li>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">Verschieben</div>
+            <div class="list-group">
+                <div class="list-group-item"
+                <li>Webentwicklung Übung 2</li>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+
+</div>
+
+</body>
+<footer>
+
+</footer>
+</html>
