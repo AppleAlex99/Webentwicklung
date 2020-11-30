7d6828296f50c9f4b1f0d0b70c910d250a6b2008
Alexander Winzig <alex.winzig@gmx.de>
2020-11-30 10:45:32 +0100

    mitlgiederArrays geaddet

---
+++ Ueb01/mitgliederArray.php
@@ -0,0 +1,23 @@
+<?php
+$mitglieder = array(
+// Erzeuge den ersten Array und speichere einige Werte darin
+    array(
+        'id' => 1,
+        'username' => 'kalenborn',
+        'email' => 'kalenborn@example.com',
+        'projektID' => 1
+    ),
+// Erzeuge den zweiten Array und speichere einige Werte darin
+
+    array(
+        'id' => 3,
+        'username' => 'elena',
+        'email' => 'elena@example.com',
+        'projektID' => 1
+    )
+);
+
+var_dump($mitglieder)
+
+
+?>
