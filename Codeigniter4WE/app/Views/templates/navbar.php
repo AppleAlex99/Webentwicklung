6b16fea37b0bc64d53f803ec03b05e48c414b4b1
Alexander Winzig <alex.winzig@gmx.de>
2020-12-15 18:15:40 +0100

    Finished Aufgabe 1, added Directory for Views

--- Codeigniter4WE/app/Views/templates/navbar.php
+++ Codeigniter4WE/app/Views/templates/navbar.php
@@ -1,28 +1,22 @@
 <nav class="navbar">
     <ul class="list-group">
         <li class="list-group-item">
-            <a href="Login.php">
-                Login
-            </a>
+            <a href="<?php echo site_url('Login/index/url') ?>">Login</a>
         </li>
         <li class="list-group-item">
-            <a href="Projekte.php">
-                Projekte
-            </a>
+            <a href="<?php echo site_url('Login/index/url') ?>">Projekte</a>
         </li>
         <li class="list-group-item">
-            <a href="index.php">
-                Aktuelles Projekt
-            </a>
+            <a href="<?php echo site_url('Login/index/url') ?>">Aktuelles Projekt</a>
         </li>
         <li class="list-group-item" style="width: 70%; margin-left: auto;text-align: center ">
-            <a href="Reiter.php">Reiter</a>
+            <a href="<?php echo site_url('Login/index/url') ?>">Reiter</a>
         </li>
         <li class="list-group-item" style="width: 70%; margin-left: auto;text-align: center ">
-            <a href="Aufgaben.php">Aufgaben</a>
+            <a href="<?php echo site_url('Login/index/url') ?>">Aufgaben</a>
         </li>
         <li class="list-group-item" style="width: 70%; margin-left: auto;text-align: center ">
-            <a href="Personen.php">Mitglieder</a>
+            <a href="<?php echo site_url('Login/index/url') ?>">Personen</a>
         </li>
     </ul>
 </nav>
